721df7800ecee3ecc8f2f405da2508e2cdbbed38
pjh <wlsgur0217@naver.com>
2022-07-26 22:57:52 +0900

    22.07.26

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,20 +1,38 @@
-var swiper = new Swiper(".mySwiper", {
-    // 하단 스크롤바 생성
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
-    },
+$(function(){
 
-    // 동시 다중 슬라이드 중앙정렬
-    slidesPerView: 4,
-    centeredSlides: true,
-    spaceBetween: 30,
-    grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  var swiper = new Swiper(".swiperBar", {
+      // 하단 스크롤바 생성
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
 
+      // 동시 다중 슬라이드 중앙정렬
+      slidesPerView: 1.5,
+      centeredSlides: false,
+      spaceBetween: 0,
+      grabCursor: true,
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      },
+
+  });
+
+  var swiper = new Swiper(".swiperFade", {
     // 슬라이드 효과
+    spaceBetween: 100,
+    loop: true,
     effect: "fade",
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+})
+
